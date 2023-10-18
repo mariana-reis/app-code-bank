609c0afbbac961409b9ad6d31886560d43a27f66
mariana-reis <mariana.reis@al.resilia.com.br>
2023-10-18 10:21:14 -0300

    :sparkles: feat:add cmd

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/jinzhu/gorm"
+	"github.com/mariana-reis/app-code-bank/application/grpc"
+	"github.com/mariana-reis/app-code-bank/infrastructure/db"
+)
+
+var database *gorm.DB
+
+func main() {
+	database = db.ConnectDB(os.Getenv("env"))
+	grpc.StartGrpcServer(database, 50051)
+}
